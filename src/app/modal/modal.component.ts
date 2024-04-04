a2b31bb55ba2ab0d9398e332e002e29f299b7fe8
Hassan Bouzlima <bouzlima.h@live.fr>
2024-04-04 22:46:56 +0100

    Add modal component

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+})
+export class ModalComponent {
+  @Input() show!: boolean;
+  @Output() dismiss = new EventEmitter();
+
+  sendDismiss(e: Event) {
+    e.stopPropagation();
+    this.dismiss.emit('dismiss');
+  }
+  clickModal(e: Event) {
+    e.stopPropagation();
+  }
+}
