ddb8c058829bc77911b6c4fc9bcdcab7587e825e
Hassan Bouzlima <bouzlima.h@live.fr>
2024-04-08 13:24:26 +0100

    Add dynamic button component

---
+++ src/app/admin/dynamic-button/dynamic-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-button',
+  templateUrl: './dynamic-button.component.html',
+  styleUrl: './dynamic-button.component.css',
+})
+export class DynamicButtonComponent {
+  @Input() isWaiting!: boolean;
+  @Input() isDisabled!: boolean;
+  @Input() label!: string;
+  @Input() classes?: string;
+  @Input() waitingLabel!: string;
+  @Output() clicked = new EventEmitter();
+  constructor() {}
+  onClick() {
+    this.clicked.emit();
+  }
+}
