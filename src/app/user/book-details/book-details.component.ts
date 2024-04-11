b6471d59f4d91ed4b5be72d6b1cc5ebefdd8c601
Hassan Bouzlima <bouzlima.h@live.fr>
2024-04-11 09:39:23 +0100

    Update book-details

--- src/app/user/book-details/book-details.component.ts
+++ src/app/user/book-details/book-details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { IUser } from '../../interfaces/IUser';
 
 @Component({
   selector: 'app-book-details',
@@ -7,8 +9,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './book-details.component.css',
 })
 export class BookDetailsComponent {
-  constructor(private route: ActivatedRoute) {}
+  show = false;
+  isAuth!: IUser | null;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
   ngOnInit(): void {
     const bookId = this.route.snapshot.paramMap.get('id');
+    this.authService.getAuthListener().subscribe((v) => {
+      this.isAuth = v;
+    });
+  }
+
+  onBorrow() {
+    if (this.isAuth) this.showModal();
+    else this.router.navigateByUrl('auth');
+  }
+
+  showModal() {
+    this.show = !this.show;
   }
 }
